158d461c1c5dc244f06f915dc96269d1d9c164d5
Marte Nordin <marte.nordin@nav.no>
2026-09-25 07:07:45 +0000

    Særbidrag: lister kolonner i viewene

--- models/BB_saerbidrag_mnd/view_fak_bb_inntekt.sql
+++ models/BB_saerbidrag_mnd/view_fak_bb_inntekt.sql
@@ -1,15 +1,29 @@
-with fak as (
+with saer as (
     select * 
     from {{ ref('fam_bb_saerbidrag_inntekt_mnd') }}
-   -- where gyldig_flagg = 1 
-   -- and aktuell_flagg = 1
+   where gyldig_flagg = 1 
 ),
 
 final as (
     select 
-        fak.*,
-        'SÆRBIDRAG' as stonad_type
-    from fak
+    KEY_FAK_BB_SAERBIDRAG,
+    VEDTAKS_ID,
+    SAKSNR,
+    VEDTAKSTIDSPUNKT,
+    TYPE_INNTEKT,
+    INNTEKT_KATEGORI,
+    VALUTA_KODE,
+    INNTEKT_MOTTAKER,
+    INNTEKT_SKYLDNER,
+    INNTEKT_KRAVHAVER,
+    INNTEKT_MOTTAKER_NOK,
+    INNTEKT_SKYLDNER_NOK,
+    INNTEKT_KRAVHAVER_NOK,
+    --GYLDIG_FLAGG,
+    MART_LASTET_DATO,
+    LASTET_DATO,
+    'SÆRBIDRAG' as stonad_type
+    from saer
     -- UNION med andre inntekter
 )
 
